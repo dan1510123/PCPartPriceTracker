73bcc1c8dee77ed92824eddb212cbff5b00faf87
Daniel Luo <dyl24@case.edu>
2020-11-29 13:58:46 -0500

    Basic PCPart and Main class created

---
+++ Main.py
@@ -0,0 +1,5 @@
+def main():
+    print("python main function")
+
+if __name__ == '__main__':
+    main()
---
+++ PCPart.py
@@ -0,0 +1,5 @@
+class PCPart:
+    def __init__(self, partName, partPrice):
+        self.name = str(partName)
+        self.price = int(partPrice)
+    
